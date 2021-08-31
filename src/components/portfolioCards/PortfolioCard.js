aaeb0148d08a602653c2fbbf3fe79f96097c1b3c
Alex Abbamondi <alex.abbamondi@gmail.com>
2021-08-31 12:20:57 -0400

    added target to open up in new page

--- src/components/portfolioCards/PortfolioCard.js
+++ src/components/portfolioCards/PortfolioCard.js
@@ -4,13 +4,13 @@ import "./PortfolioCard.css"
 const PortfolioCard = (props) => {
     return (
         <div className="col d-flex justify-content-center mb-5">
-            <a href={props.link}>
+            <a href={props.link} target="_blank" rel="noopener noreferrer">
             <div className="works">
               <img src={props.img} alt="" className="work-img"/>
               <div className="works-title">
                 <h3>{props.title}</h3>
                 <p>{props.stack}</p>
-                <button><a href={props.github}>Github</a></button>
+                <button><a href={props.github} target="_blank" rel="noopener noreferrer">Github</a></button>
               </div>
             </div>
           </a>
